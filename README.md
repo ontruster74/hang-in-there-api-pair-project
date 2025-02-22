f49ff627140ee2957305cd5c95c5eacb98a8fafe
ontruster74 <ontruster74@gmail.com>
2025-02-22 00:20:51 -0700

    fill out project README

--- README.md
+++ README.md
@@ -1,24 +1,37 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+PAIR PROJECT - HANG IN THERE API
+-------------------------------------------------
 
-Things you may want to cover:
+COLLABORATORS
+------------------------------
+Donte Handy
+
+GitHub Profile: https://github.com/dontehandy/
+
+Rig Freyr 
+
+GitHub Profile: https://github.com/ontruster74
+
+---------------------------------
+SETUP
+---------------------------------
 
 * Ruby version
-
-* System dependencies
+    At least Ruby version 3.2.2 and Ruby on Rails 7.1.X is required for this project to function properly.
 
 * Configuration
+    The "rspec-rails," "pry," and "simplecov" gems are included in this project. Run $ bundle install in the project directory once you have cloned it down to your machine to ensure they function properly.
 
-* Database creation
+* Database creation/initialization
+    Run $rails db:{drop,create,migrate,seed} to prepare the database for use.
 
-* Database initialization
+    Then run $ rails console or $ rails dbconsole to interact with the database directly, or use $ rails s to start a rails server and test the project's functionality in the browser of your choice.
 
 * How to run the test suite
+    Run $ bundle exec rspec spec/requests/api/v1/posters_request_spec.rb to run the rspec test suite.
 
-* Services (job queues, cache servers, search engines, etc.)
+    To test the project's endpoints in Postman, import the following test suite: https://curriculum.turing.edu/module2/projects/hang_in_there_api/hang_in_there_api.postman_collection.json
 
-* Deployment instructions
+    Then run $ rails s to start a rails server and send each of the provided test suite requests to the project individually. Only the first 11 requests are supported - Extensions/Sad Path functionality is not supported.
 
-* ...
--- spec/requests/api/v1/posters_request_spec.rb
+++ spec/requests/api/v1/posters_request_spec.rb
@@ -137,70 +137,155 @@ describe "Posters API", type: :request do
   end
 
   it "can sort returned posters in ascending order by created_at" do
-    Poster.create(name: "Poster A", price: 10.0, year: 2000, vintage: false, img_url: "url1")
-    Poster.create(name: "Poster B", price: 20.0, year: 1990, vintage: true, img_url: "url2")
-    Poster.create(name: "Poster C", price: 15.0, year: 2010, vintage: false, img_url: "url3")
+    Poster.create(name: "FAILURE",
+      description: "Why bother trying? It's probably not worth it.",
+      price: 68.00,
+      year: 2019,
+      vintage: true,
+      img_url:  "https://images.unsplash.com/photo-1620401537439-98e94c004b0d")
 
-    get "/api/v1/posters", params: { sort: 'asc' }
+    Poster.create(name: "REGRET",
+      description: "Hard work rarely pays off.",
+      price: 89.00,
+      year: 2018,
+      vintage: true,
+      img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d")
+
+    Poster.create(name: "MEDIOCRITY",
+      description: "Dreams are just that-dreams.",
+      price: 127.00,
+      year: 2021,
+      vintage: false,
+      img_url:  "https://images.unsplash.com/photo-1551993005-75c4131b6bd8")
+
+    get "/api/v1/posters?sort=asc"
 
     expect(response).to be_successful
 
     posters = JSON.parse(response.body, symbolize_names: true)[:data]
-    expect(posters.map { |poster| poster[:attributes][:name] }).to eq(["Poster A", "Poster B", "Poster C"])
+    expect(posters.map { |poster| poster[:attributes][:name] }).to eq(["FAILURE", "REGRET", "MEDIOCRITY"])
   end
 
   it "can sort returned posters in descending order by created_at" do
-    Poster.create(name: "Poster A", price: 10.0, year: 2000, vintage: false, img_url: "url1")
-    Poster.create(name: "Poster B", price: 20.0, year: 1990, vintage: true, img_url: "url2")
-    Poster.create(name: "Poster C", price: 15.0, year: 2010, vintage: false, img_url: "url3")
+    Poster.create(name: "FAILURE",
+    description: "Why bother trying? It's probably not worth it.",
+    price: 68.00,
+    year: 2019,
+    vintage: true,
+    img_url:  "https://images.unsplash.com/photo-1620401537439-98e94c004b0d")
 
-    get "/api/v1/posters", params: { sort: 'desc' }
+  Poster.create(name: "REGRET",
+    description: "Hard work rarely pays off.",
+    price: 89.00,
+    year: 2018,
+    vintage: true,
+    img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d")
+
+  Poster.create(name: "MEDIOCRITY",
+    description: "Dreams are just that-dreams.",
+    price: 127.00,
+    year: 2021,
+    vintage: false,
+    img_url:  "https://images.unsplash.com/photo-1551993005-75c4131b6bd8")
+
+    get "/api/v1/posters?sort=desc"
 
     expect(response).to be_successful
 
     posters = JSON.parse(response.body, symbolize_names: true)[:data]
-    expect(posters.map { |poster| poster[:attributes][:name] }).to eq(["Poster C", "Poster B", "Poster A"])
+    expect(posters.map { |poster| poster[:attributes][:name] }).to eq(["MEDIOCRITY", "REGRET", "FAILURE"])
   end
 
   it "can filter returned posters by name" do
-    Poster.create(name: "Poster A", price: 10.0, year: 2000, vintage: false, img_url: "url1")
-    Poster.create(name: "Poster B", price: 20.0, year: 1990, vintage: true, img_url: "url2")
-    Poster.create(name: "Poster C", price: 15.0, year: 2010, vintage: false, img_url: "url3")
+    Poster.create(name: "FAILURE",
+    description: "Why bother trying? It's probably not worth it.",
+    price: 68.00,
+    year: 2019,
+    vintage: true,
+    img_url:  "https://images.unsplash.com/photo-1620401537439-98e94c004b0d")
 
-    get "/api/v1/posters", params: { name: 'Poster B' }
+  Poster.create(name: "REGRET",
+    description: "Hard work rarely pays off.",
+    price: 89.00,
+    year: 2018,
+    vintage: true,
+    img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d")
+
+  Poster.create(name: "MEDIOCRITY",
+    description: "Dreams are just that-dreams.",
+    price: 127.00,
+    year: 2021,
+    vintage: false,
+    img_url:  "https://images.unsplash.com/photo-1551993005-75c4131b6bd8")
+
+    get "/api/v1/posters?name=re"
 
     expect(response).to be_successful
 
     posters = JSON.parse(response.body, symbolize_names: true)[:data]
-    expect(posters.count).to eq(1)
-    expect(posters.first[:attributes][:name]).to eq("Poster B")
+    expect(posters.count).to eq(2)
+    expect(posters.map { |poster| poster[:attributes][:name] }).to eq(["FAILURE", "REGRET"])
   end
 
   it "can filter returned posters by minimum price" do
-     Poster.create(name: "Poster A", price: 10.0, year: 2000, vintage: false, img_url: "url1")
-     Poster.create(name: "Poster B", price: 20.0, year: 1990, vintage: true, img_url: "url2")
-     Poster.create(name: "Poster C", price: 15.0, year: 2010, vintage: false, img_url: "url3")
+    Poster.create(name: "FAILURE",
+    description: "Why bother trying? It's probably not worth it.",
+    price: 68.00,
+    year: 2019,
+    vintage: true,
+    img_url:  "https://images.unsplash.com/photo-1620401537439-98e94c004b0d")
 
-     get "/api/v1/posters", params: { min_price: 15.0 }
+  Poster.create(name: "REGRET",
+    description: "Hard work rarely pays off.",
+    price: 89.00,
+    year: 2018,
+    vintage: true,
+    img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d")
+
+  Poster.create(name: "MEDIOCRITY",
+    description: "Dreams are just that-dreams.",
+    price: 127.00,
+    year: 2021,
+    vintage: false,
+    img_url:  "https://images.unsplash.com/photo-1551993005-75c4131b6bd8")
+
+     get "/api/v1/posters?min_price=88.00"
 
      expect(response).to be_successful
 
      posters = JSON.parse(response.body, symbolize_names: true)[:data]
      expect(posters.count).to eq(2)
-     expect(posters.map { |poster| poster[:attributes][:price] }).to eq([20.0, 15.0])
+     expect(posters.map { |poster| poster[:attributes][:price] }).to eq([89.00, 127.00])
   end
 
   it "can filter returned posters by maximum price" do
-     Poster.create(name: "Poster A", price: 10.0, year: 2000, vintage: false, img_url: "url1")
-     Poster.create(name: "Poster B", price: 20.0, year: 1990, vintage: true, img_url: "url2")
-     Poster.create(name: "Poster C", price: 15.0, year: 2010, vintage: false, img_url: "url3")
+    Poster.create(name: "FAILURE",
+    description: "Why bother trying? It's probably not worth it.",
+    price: 68.00,
+    year: 2019,
+    vintage: true,
+    img_url:  "https://images.unsplash.com/photo-1620401537439-98e94c004b0d")
 
-     get "/api/v1/posters", params: { max_price: 15.0 }
+  Poster.create(name: "REGRET",
+    description: "Hard work rarely pays off.",
+    price: 89.00,
+    year: 2018,
+    vintage: true,
+    img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d")
+
+  Poster.create(name: "MEDIOCRITY",
+    description: "Dreams are just that-dreams.",
+    price: 127.00,
+    year: 2021,
+    vintage: false,
+    img_url:  "https://images.unsplash.com/photo-1551993005-75c4131b6bd8")
+
+     get "/api/v1/posters?max_price=88.00"
 
      expect(response).to be_successful
 
      posters = JSON.parse(response.body, symbolize_names: true)[:data]
-     expect(posters.count).to eq(2)
-     expect(posters.map { |poster| poster[:attributes][:price] }).to eq([10.0, 15.0])
+     expect(posters.count).to eq(1)
+     expect(posters.map { |poster| poster[:attributes][:price] }).to eq([68.00])
   end
 end
